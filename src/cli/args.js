bdb75ee5bdd85a27daf512458114d3607cb7cb3c
aliaksei.volkav <aliaksei.volkav@ventionteams.com>
2024-01-28 18:53:29 +0100

    [#2] Add Command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const { argv } = process;
+  const args = argv.slice(2);
+  const list = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].slice(2);
+    const value = args[i + 1];
+    list.push(`${key} as ${value}`);
+  }
+
+  console.log(list.join(", "));
 };
 
 parseArgs();
