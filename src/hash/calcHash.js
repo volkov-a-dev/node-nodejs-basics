48e21c3c494907cc54dabf0e83265b254e43669f
aliaksei.volkav <aliaksei.volkav@ventionteams.com>
2024-01-28 21:33:08 +0100

    [#4] Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import { getFilePath } from "../utils/index.js";
+
+const FILES_PATH = getFilePath(
+  import.meta.url,
+  "files",
+  "fileToCalculateHashFor.txt"
+);
+
+const calculateHash = async (filename, hashAlgorithm = "sha256") => {
+  // Write your code here
+  const hash = createHash(hashAlgorithm);
+  const input = createReadStream(filename);
+  input.on("data", (data) => hash.update(data));
+  input.on("end", () => {
+    const hex = hash.digest("hex");
+    console.log(hex);
+  });
 };
 
-await calculateHash();
+await calculateHash(FILES_PATH);
