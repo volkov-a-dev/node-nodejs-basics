a7308ba3d30e78a1365ba767cd1beffa32b49d2b
aliaksei.volkav <aliaksei.volkav@ventionteams.com>
2024-01-29 23:08:36 +0100

    [#8] Worker Threads (src/wt)

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
+import { getFilePath } from "../utils/index.js";
+
+const WORKER_FILE_PATH = getFilePath(import.meta.url, "", "worker.js");
+const CPUS_COUNT = cpus().length;
+
+const serviceWorker = (number, workerFilePath) =>
+  new Promise((resolve, _) => {
+    console.log("data", number);
+    const worker = new Worker(workerFilePath, {
+      workerData: number,
+    });
+
+    worker.on("message", (res) => resolve({ status: "success", data: res }));
+    worker.on("error", () => resolve({ status: "error", data: null }));
+  });
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  const data = Array.from({ length: CPUS_COUNT }, (_, i) =>
+    serviceWorker(10 + i, WORKER_FILE_PATH)
+  );
+  const workers = await Promise.all(data);
+
+  console.log(workers);
 };
 
 await performCalculations();
